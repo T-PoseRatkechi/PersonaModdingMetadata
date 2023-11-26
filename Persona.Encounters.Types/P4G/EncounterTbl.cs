5ad1cd490e9dc94b4e27e62a4a4c3d1fde8a7964
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-26 17:33:16 -0600

    Add P4G encount TBL and types.

---
+++ Persona.Encounters.Types/P4G/EncounterTbl.cs
@@ -0,0 +1,16 @@
+﻿using Persona.Encounters.Types.Common;
+
+namespace Persona.Encounters.Types.P4G;
+
+public class EncounterTbl : BaseEncounterTbl<BattleUnit, Music>
+{
+    public EncounterTbl(string file)
+        : base(file, 24)
+    {
+    }
+
+    public EncounterTbl(Stream stream)
+        : base(stream, 24)
+    {
+    }
+}
---
+++ Persona.Encounters.Types/P4G/Music.cs
@@ -0,0 +1,18 @@
+﻿namespace Persona.Encounters.Types.P4G;
+
+public enum Music
+	: ushort
+{
+    TimeToMakeHistory = 0,
+	ReachOutToTheTruth = 1,
+	FaceMyself = 2,
+	NewWorldFool = 3,
+	Fog = 4,
+	TheAlmighty = 6,
+	BorderlineOfMadness = 7,
+	Reverie = 8,
+	ReachOutToTheTruthFirstBattle = 9,
+	FaceMyself8bit = 11,
+	FaceMyselfRemix1 = 12,
+	FaceMyselfRemix2 = 13,
+}

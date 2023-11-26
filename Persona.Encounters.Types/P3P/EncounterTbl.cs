764bbecdd8405b300ebb79af1725d95ecb53adb0
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-26 17:32:41 -0600

    Add P3P encounter TBL and types.

---
+++ Persona.Encounters.Types/P3P/EncounterTbl.cs
@@ -0,0 +1,16 @@
+﻿using Persona.Encounters.Types.Common;
+
+namespace Persona.Encounters.Types.P3P;
+
+public class EncounterTbl : BaseEncounterTbl<BattleUnits, Music>
+{
+    public EncounterTbl(string file)
+        : base(file, 28)
+    {
+    }
+
+    public EncounterTbl(Stream stream)
+        : base(stream, 28)
+    {
+    }
+}
---
+++ Persona.Encounters.Types/P3P/Music.cs
@@ -0,0 +1,12 @@
+﻿namespace Persona.Encounters.Types.P3P;
+
+public enum Music
+    : ushort
+{
+    MassDestruction = 0,
+    MasterOfShadow = 1,
+    UnavoidableBattle = 2,
+    MasterOfTartarus = 3,
+    HeartfulCry = 7,
+    Darkness = 8,
+}

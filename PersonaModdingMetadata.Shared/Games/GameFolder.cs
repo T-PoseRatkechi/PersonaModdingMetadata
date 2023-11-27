7b43c1c6d6ea86efd17af703fe3e084d24d3a222
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-26 22:53:17 -0600

    Update game folder extensions.

--- PersonaModdingMetadata.Shared/Games/GameFolder.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PersonaModdingMetadata.Shared.Games;
-
-public static class GameFolder
-{
-    public static string OriginalFilesDir(this Game game) => Path.Join(game.ToString(), "files", "original");
-}

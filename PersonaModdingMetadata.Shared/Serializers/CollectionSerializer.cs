db560e2c49e5caebe46f5907c36225f772d09585
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-27 00:20:18 -0600

    Rework collection serializer.

---
+++ PersonaModdingMetadata.Shared/Serializers/CollectionSerializer.cs
@@ -0,0 +1,24 @@
+﻿namespace PersonaModdingMetadata.Shared.Serializers;
+
+public static class CollectionSerializer
+{
+    public static string Serialize<T>(IEnumerable<T> items)
+        where T : notnull
+        => string.Join('\n', items.Select(x => x.ToString()));
+
+    public static int[] Deserialize(string text)
+    {
+        var items = text.Split('\n')
+            .Where(x => !x.StartsWith("//") && !string.IsNullOrEmpty(x))
+            .Select(int.Parse)
+            .ToArray();
+
+        return items;
+    }
+
+    public static void Serialize<T>(string file, IEnumerable<T> items)
+        where T : notnull
+        => File.WriteAllText(file, Serialize(items));
+
+    public static int[] DeserializeFile(string file) => Deserialize(File.ReadAllText(file));
+}

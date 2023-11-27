0b1ef59dd0ce1d65668f52e33d30528ce2f12d86
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-27 02:31:00 -0600

    Handle secondary new line character. Create output file directory when serializing to file.

--- PersonaModdingMetadata.Shared/Serializers/CollectionSerializer.cs
+++ PersonaModdingMetadata.Shared/Serializers/CollectionSerializer.cs
@@ -8,7 +8,7 @@ public static class CollectionSerializer
 
     public static int[] Deserialize(string text)
     {
-        var items = text.Split('\n')
+        var items = text.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
             .Where(x => !x.StartsWith("//") && !string.IsNullOrEmpty(x))
             .Select(int.Parse)
             .ToArray();
@@ -16,9 +16,13 @@ public static class CollectionSerializer
         return items;
     }
 
-    public static void Serialize<T>(string file, IEnumerable<T> items)
+    public static void Serialize<T>(string filePath, IEnumerable<T> items)
         where T : notnull
-        => File.WriteAllText(file, Serialize(items));
+    {
+        var file = new FileInfo(filePath);
+        file.Directory?.Create();
+        File.WriteAllText(file.FullName, Serialize(items));
+    }
 
     public static int[] DeserializeFile(string file) => Deserialize(File.ReadAllText(file));
 }

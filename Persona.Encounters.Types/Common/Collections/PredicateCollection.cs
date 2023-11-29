5723f88858cc19c7f643afbc9f0a9a54b1cd2a24
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-11-28 19:49:06 -0600

    Add collection type that takes a predicate for matching.

---
+++ Persona.Encounters.Types/Common/Collections/PredicateCollection.cs
@@ -0,0 +1,12 @@
+﻿namespace Persona.Encounters.Types.Common.Collections;
+
+public class PredicateCollection<TEncounter> : IEncounterFilter<TEncounter>
+{
+    private readonly Func<TEncounter, bool> predicate;
+
+    public PredicateCollection(Func<TEncounter, bool> predicate)
+        => this.predicate = predicate;
+
+    public bool Match(TEncounter encounter)
+        => this.predicate(encounter);
+}

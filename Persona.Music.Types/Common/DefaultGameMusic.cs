8455182aa33d1e5a87b66012929d368c35828ab2
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-01 06:09:16 -0600

    Add function for getting default music.

---
+++ Persona.Music.Types/Common/DefaultGameMusic.cs
@@ -0,0 +1,15 @@
+﻿using PersonaModdingMetadata.Shared.Games;
+
+namespace Persona.Music.Types.Common;
+
+public static class DefaultGameMusic
+{
+    private static readonly Dictionary<Game, GameMusic> music = new()
+    {
+        [Game.P4G_PC] = new P4G.DefaultSongs(),
+        [Game.P5R_PC] = new P5R.DefaultSongs(),
+        [Game.P3P_PC] = new P3P.DefaultSongs(),
+    };
+
+    public static GameMusic For(Game game) => music[game];
+}

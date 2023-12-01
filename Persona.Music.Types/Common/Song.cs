886a95ff3e790c7b2735ac3f19866e342a00c46f
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-01 06:11:17 -0600

    Remove pointless id property from songs.

--- Persona.Music.Types/Common/Song.cs
+++ Persona.Music.Types/Common/Song.cs
@@ -2,8 +2,6 @@
 
 public class Song
 {
-    public string Id { get; set; } = Guid.NewGuid().ToString();
-
     public string Name { get; set; } = string.Empty;
 
     public string Category { get; set; } = string.Empty;

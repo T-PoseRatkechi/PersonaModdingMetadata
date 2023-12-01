dfcc0b3a3be1739d73703d81cb6658c4d6d3732b
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-01 06:12:29 -0600

    Add default music tests.

---
+++ Persona.Music.Types.Tests/DefaultMusicTests.cs
@@ -0,0 +1,43 @@
+﻿using Persona.Music.Types.Common;
+using PersonaModdingMetadata.Shared.Games;
+
+namespace Persona.Music.Types.Tests;
+
+public class DefaultMusicTests
+{
+    [Fact]
+    public void DefaultMusic_NoDuplicate_Names()
+    {
+        foreach (var game in Enum.GetValues<Game>())
+        {
+            var defaultMusic = DefaultGameMusic.For(game);
+            var currentSong = string.Empty;
+
+            Assert.True(defaultMusic.Songs
+                .GroupBy(x => x.Name)
+                .All(x =>
+                {
+                    currentSong = x.Key;
+                    return x.Count() == 1;
+                }), $"{game}: {currentSong}");
+        }
+    }
+
+    [Fact]
+    public void DefaultMusic_NoDuplicate_ReplacementPaths()
+    {
+        foreach (var game in Enum.GetValues<Game>())
+        {
+            var defaultMusic = DefaultGameMusic.For(game);
+            var currentSong = string.Empty;
+
+            Assert.True(defaultMusic.Songs
+                .GroupBy(x => x.ReplacementPath)
+                .All(x =>
+                {
+                    currentSong = x.Key;
+                    return x.Count() == 1;
+                }), $"{game}: {currentSong}");
+        }
+    }
+}

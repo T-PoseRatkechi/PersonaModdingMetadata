793c24e341e4eef39304ab3c3869518448ae8d11
T-Pose Ratkechi <tposeratkechi@gmail.com>
2023-12-01 06:25:31 -0600

    Add music metadata generator.

---
+++ PersonaModdingMetadata.CLI/Music/MusicMetadata.cs
@@ -0,0 +1,32 @@
+﻿using Persona.Music.Types.Common;
+using PersonaModdingMetadata.CLI.Common;
+using PersonaModdingMetadata.Shared.Games;
+using Serilog;
+
+namespace PersonaModdingMetadata.CLI.Music;
+
+internal class MusicMetadata : IMetadata
+{
+    private readonly string baseDir;
+
+    public MusicMetadata(string solutionDir)
+    {
+        this.baseDir = Path.Join(solutionDir, "Persona.Music.Metadata"); ;
+    }
+
+    public void Generate()
+    {
+        var serializer = new YamlDotNet.Serialization.SerializerBuilder()
+            .DisableAliases()
+            .Build();
+
+        foreach (var game in Enum.GetValues<Game>())
+        {
+            var musicFile = Path.Join(game.GameFolder(this.baseDir), "music.yaml");
+            var defaultMusic = DefaultGameMusic.For(game);
+            var yaml = serializer.Serialize(defaultMusic);
+            File.WriteAllText(musicFile, yaml);
+            Log.Logger.CreatedFile(musicFile, "Created default music for: {game}", game);
+        }
+    }
+}
